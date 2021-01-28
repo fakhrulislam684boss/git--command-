16de2701607114dd96bdb84773ba5f79fe766ddb
Md Fakhrul Islam <fakhrul33-684@diu.edu.bd>
2021-01-29 02:38:19 +0600

    Create git.js

---
+++ git.js
@@ -0,0 +1,8 @@
+//  for using git first of all have to install git software from offcial website 
+//  to check successfully git install or not , just go to desktop and press mouse left button and 
+// check have GUI Base or not
+
+// for using git firstly have to do just some formarlity using any coommand Promt 
+
+git --version // or 
+git -v // for check git version 
