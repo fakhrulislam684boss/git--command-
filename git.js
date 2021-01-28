ca9b9ee0e9d4dee4da2702ae5d80bb634d5a2a72
Md Fakhrul Islam <fakhrul33-684@diu.edu.bd>
2021-01-29 04:04:03 +0600

    Update git.js

--- git.js
+++ git.js
@@ -6,3 +6,34 @@
 
 git --version // or 
 git -v // for check git version 
+
+// ===  setting git =====
+
+git config  --global user.name "Md Fakhrul Islam " // set Name as commit point 
+git config  --global user.email "1nayan122@gmail.com" // set email in git 
+git config --list  // for check set Name and  Email 
+
+// ==== InitiaLization  git 
+
+git init // initialization as a git reposiory 
+// when we init git at that time a MASTER  BRANCH  will open in git folder and this branch will track all version change
+
+git status // for check the situation the git reposiory 
+
+git add <fileName> // or 
+git add index.js // for stages single file *** before commited , firstly have to stages *****
+git rm --cached <file> // to unstages single file 
+git add . // for staging all find and then redy to be commited 
+git commit -m "First Commit" // for commit 
+git log // for checking commit and can see commit address 
+git log --oneline  // shortcut checking target result **commit address used for going to one commit to another commit . thats mean one version to another version 
+git checkout commitAddress fileName // or
+git checkout 5455dje  index.js  // for change according to commit address 
+git reset index.js // to unstages 
+git checkout -- index.js // to go latest version 
+
+// ===== gitignore =======
+// to gitignore first of all have to create .gitignore  file 
+// gitignore not working on previous commited file
+// to ignore file just type file name in ignore file     . such as index.js
+// to ignore directory/ foldeer just use / before directory name . such as /folderName
